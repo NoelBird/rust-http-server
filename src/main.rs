f66b69cdd8b81af53b545b6099f6f806f1bc8442
noelbird <lduldu00228@gmail.com>
2024-09-18 02:16:05 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod response;
 use std::io::{BufRead, BufReader, Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread::spawn;
 use crate::response::Response;
 
 
@@ -15,45 +17,50 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                let mut buf_reader = BufReader::new(&mut _stream);
-                let request_line = buf_reader.by_ref().lines().next().unwrap().unwrap();
-                let uri: Vec<&str> = request_line.split(" ").collect();
-
-                let mut res = match uri[1] {
-                    "/" => { Response::new(&"200", &"OK", "")
-                    },
-                    res if res.starts_with("/echo/") => {
-                        let parameter: Vec<&str> = res.split("/echo/").collect();
-                        Response::new(&"200", &"OK", parameter[1])
-                    },
-                    res if res.starts_with("/user-agent") => {
-                        let mut user_agent = String::new();
-                        for line in buf_reader.by_ref().lines() {
-                            let line = line.unwrap();
-                            if line == "\r\n" || line.is_empty() {
-                                break;
-                            }
-
-                            if line.starts_with("User-Agent: ") {
-                                user_agent = line.split("User-Agent: ").collect()
-                            }
-                        }
-                        Response::new(&"200", &"OK", user_agent.as_str())
-                    },
-                    _ =>  {
-                        Response::new(&"404", &"Not Found", "")
-                    },
-                };
-
-                // run command
-                _stream.write(res.build().as_slice())
-                    .expect("panic message!");
+                spawn(|| handle_connection(_stream));
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut _stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&mut _stream);
+    let request_line = buf_reader.by_ref().lines().next().unwrap().unwrap();
+    let uri: Vec<&str> = request_line.split(" ").collect();
+
+    let mut res = match uri[1] {
+        "/" => {
+            Response::new(&"200", &"OK", "")
+        }
+        res if res.starts_with("/echo/") => {
+            let parameter: Vec<&str> = res.split("/echo/").collect();
+            Response::new(&"200", &"OK", parameter[1])
+        }
+        res if res.starts_with("/user-agent") => {
+            let mut user_agent = String::new();
+            for line in buf_reader.by_ref().lines() {
+                let line = line.unwrap();
+                if line == "\r\n" || line.is_empty() {
+                    break;
+                }
+
+                if line.starts_with("User-Agent: ") {
+                    user_agent = line.split("User-Agent: ").collect()
+                }
+            }
+            Response::new(&"200", &"OK", user_agent.as_str())
+        }
+        _ => {
+            Response::new(&"404", &"Not Found", "")
+        }
+    };
+
+    // run command
+    _stream.write(res.build().as_slice())
+        .expect("panic message!");
 }
